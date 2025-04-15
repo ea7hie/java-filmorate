0bb019a464212f6ffce3904361c1462ccfd8c7f5
ea7hie <ea7hie@mail.ru>
2025-04-15 21:28:43 +0300

    ФЗ 12 спринта. Ревью №2. Версия 3.5.1 checkStyle

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.model.MpaRatings;
 import ru.yandex.practicum.filmorate.service.MpaService;
 
